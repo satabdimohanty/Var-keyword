62fca931579aece4f22816bd1f1473c19bfff0c7
SATABDI MOHANTY <137990462+satabdimohanty@users.noreply.github.com>
2023-10-29 08:14:55 +0530

    Add files via upload

---
+++ Var.java
@@ -0,0 +1,9 @@
+public class Var {
+    
+    public static void main(String[] args) {
+        var a=90;
+        var b="here i'm using var keyword";
+        System.out.println("print a " +a +" printing String " + b);
+        //remembor var keyword can't be a instance variable 
+    }
+}
